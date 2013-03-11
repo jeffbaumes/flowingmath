525c6cd320cf3409ff1de122df7540c283bd09b0
Jeffrey Baumes <jeff.baumes@kitware.com>
2013-03-11 00:14:23 -0400

    Adding some number representations

--- js/main.js
+++ js/main.js
@@ -19,34 +19,115 @@ function go(event, id) {
     return false;
 }
 
+function number(spec) {
+    "use strict";
+
+    var that,
+        value,
+        mode,
+        base,
+        pos,
+        parent,
+        element,
+        modes;
+
+    function numeralText() {
+        return value;
+    }
+
+    function wordsText() {
+        var ones,
+            tens,
+            val,
+            digits;
+        ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+        tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+        val = value;
+        digits = [];
+        while (val > 10) {
+            digits.push(value % 10);
+            val = Math.floor(val / 10);
+        }
+        digits.push(val);
+        if (value === 0) {
+            return "zero";
+        }
+        if (value < 20) {
+            return ones[value];
+        }
+        if (value < 100) {
+            return tens[digits[1]] + (digits[0] === 0 ? "" : "-" + ones[digits[0]]);
+        }
+        return "big number";
+    }
+
+    value = spec.value || 0;
+    mode = spec.mode || "boxes";
+    base = spec.base || 10;
+    pos = spec.pos || [100, 100];
+    parent = spec.parent || d3.select("svg");
+    element = parent.append("g")
+        .attr("transform", "translate(" + pos[0] + "," + pos[1] + ")");
+    modes = {};
+    modes.boxes = element.append("g").style("opacity", 0);
+    modes.boxes.selectAll("g.boxes").data(d3.range(value))
+        .enter().append("g")
+        .attr("class", "boxes")
+        .style("opacity", "inherit")
+        .attr("transform", function (d) { return "translate(" + 50 * (d - (value - 1) / 2) + ", 0)"; })
+        .append("rect")
+        //.attr("class", "wiggle")
+        .attr("x", -20)
+        .attr("y", -20)
+        .attr("rx", 5)
+        .attr("ry", 5)
+        .attr("width", 40)
+        .attr("height", 40)
+        .style("opacity", "inherit");
+    modes.numeral = element.append("text")
+        .attr("class", "numeral")
+        .text(numeralText())
+        .style("opacity", 0);
+    modes.words = element.append("text")
+        .attr("class", "words")
+        .text(wordsText())
+        .style("opacity", 0);
+
+    modes.boxes.transition().duration(4000)
+        .style("opacity", 1);
+
+    that = {};
+
+    that.mode = function (m) {
+        if (m === undefined) {
+            return mode;
+        }
+        if (m === mode) {
+            return;
+        }
+        modes[mode].transition().duration(1000)
+            .style("opacity", 0);
+        modes[m].transition().duration(1000)
+            .style("opacity", 1);
+        mode = m;
+    };
+
+    return that;
+}
+
 function goto(id, name) {
     "use strict";
 
     console.log("goto: " + id + ", " + name);
     d3.selectAll(".nav li").classed("active", function () { return d3.select(this).attr("id") === name; });
     if (name === "numbers-intro") {
+        var one;
+
         d3.select("#content").selectAll("*").remove();
+        one = number({value: 99, parent: d3.select("#content"), pos: [200, 200]});
 
-        var actors = d3.select("#content").append("g")
-            .attr("class", "actor")
-            .attr("transform", "translate(" + 100 + "," + 250 + ")");
-
-        var rect = actors.append("rect")
-            .attr("width", 100)
-            .attr("height", 100)
-            .attr("x", -50)
-            .attr("y", -50)
-            .style("opacity", 0);
-
-        actors.transition().duration(5000)
-            .attr("transform", "translate(" + 250 + "," + 250 + ")");
-
-        rect.transition().duration(5000)
-            .style("opacity", 1);
-
-        d3.timer(function (t) {
-            rect.attr("transform", "rotate(" + t / 40 + ")");
-        });
+        $("#mode").change(function () { one.mode($(this).val()); });
     }
 }
 
@@ -66,5 +147,8 @@ $(function () {
 
     Backbone.history.start({
         root: "/"
+    });
+    d3.timer(function (t) {
+        d3.selectAll(".wiggle").attr("transform", "rotate(" + 5 * Math.cos(Math.PI / 180 * t / 5) + ")");
     });
 });
