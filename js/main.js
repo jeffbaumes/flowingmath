0cebf543cda17dc9250326b8db3492be8700d579
Jeffrey Baumes <jeff.baumes@kitware.com>
2013-03-12 21:55:10 -0400

    Adding actor superclass

--- js/main.js
+++ js/main.js
@@ -19,17 +19,56 @@ function go(event, id) {
     return false;
 }
 
-function number(spec) {
+function actor(spec, my) {
+    "use strict";
+
+    var that,
+        pos,
+        parent;
+
+    my = my || {};
+    my.width = my.width || function () { return 0; };
+    my.height = my.height || function () { return 0; };
+
+    that = {};
+
+    parent = spec.parent || d3.select("svg");
+    pos = spec.pos || [0, 0];
+    my.element = parent.append("g")
+        .attr("transform", "translate(" + pos[0] + "," + pos[1] + ")");
+
+    that.pos = function (p) {
+        if (p === undefined) {
+            return pos;
+        }
+        if (p[0] === pos[0] && p[1] === pos[1]) {
+            return;
+        }
+        pos = p;
+
+        my.element.transition().duration(500)
+            .attr("transform", "translate(" + pos[0] + "," + pos[1] + ")");
+    };
+
+    return that;
+}
+
+function number(spec, my) {
     "use strict";
 
     var that,
         value,
         mode,
         base,
-        pos,
-        parent,
-        element,
         modes;
+
+    my = my || {};
+
+    my.size = function () {
+        return [value * 50, 50];
+    };
+
+    that = actor(spec, my);
 
     function numeralText() {
         return value;
@@ -59,25 +98,24 @@ function number(spec) {
         if (value < 100) {
             return tens[digits[1]] + (digits[0] === 0 ? "" : "-" + ones[digits[0]]);
         }
+        if (value < 1000) {
+            return ones[digits[2]] + "-hundred " + (digits[1] === 0 ? "" : tens[digits[1]]) + (digits[0] === 0 ? "" : "-" + ones[digits[0]]);
+        }
         return "big number";
     }
 
     value = spec.value || 0;
     mode = spec.mode || "boxes";
     base = spec.base || 10;
-    pos = spec.pos || [100, 100];
-    parent = spec.parent || d3.select("svg");
-    element = parent.append("g")
-        .attr("transform", "translate(" + pos[0] + "," + pos[1] + ")");
     modes = {};
-    modes.boxes = element.append("g").style("opacity", 0);
+    modes.boxes = my.element.append("g").style("opacity", 0);
     modes.boxes.selectAll("g.boxes").data(d3.range(value))
         .enter().append("g")
         .attr("class", "boxes")
         .style("opacity", "inherit")
         .attr("transform", function (d) { return "translate(" + 50 * (d - (value - 1) / 2) + ", 0)"; })
         .append("rect")
-        //.attr("class", "wiggle")
+        .attr("class", "wiggle")
         .attr("x", -20)
         .attr("y", -20)
         .attr("rx", 5)
@@ -85,19 +123,17 @@ function number(spec) {
         .attr("width", 40)
         .attr("height", 40)
         .style("opacity", "inherit");
-    modes.numeral = element.append("text")
+    modes.numeral = my.element.append("text")
         .attr("class", "numeral")
         .text(numeralText())
         .style("opacity", 0);
-    modes.words = element.append("text")
+    modes.words = my.element.append("text")
         .attr("class", "words")
         .text(wordsText())
         .style("opacity", 0);
 
-    modes.boxes.transition().duration(4000)
+    modes.boxes.transition().duration(1000)
         .style("opacity", 1);
-
-    that = {};
 
     that.mode = function (m) {
         if (m === undefined) {
@@ -122,12 +158,14 @@ function goto(id, name) {
     console.log("goto: " + id + ", " + name);
     d3.selectAll(".nav li").classed("active", function () { return d3.select(this).attr("id") === name; });
     if (name === "numbers-intro") {
-        var one;
+        var one, two;
 
         d3.select("#content").selectAll("*").remove();
-        one = number({value: 99, parent: d3.select("#content"), pos: [200, 200]});
+        one = number({value: 1, parent: d3.select("#content"), pos: [200, 200]});
+        two = number({value: 2, parent: d3.select("#content"), pos: [100, 200]});
 
         $("#mode").change(function () { one.mode($(this).val()); });
+        $("#move").click(function () { one.pos([100 * Math.random(), 100 * Math.random()]); });
     }
 }
 
